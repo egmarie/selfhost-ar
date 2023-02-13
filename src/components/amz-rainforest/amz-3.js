3ca42b4c4c46f58dff755a3ba9fd1f4dbafc738a
egmarie <egmarie@MacBook-Pro-409.lan>
2023-02-13 13:59:24 -0500

    update ec2 again

--- src/components/amz-rainforest/amz-3.js
+++ src/components/amz-rainforest/amz-3.js
@@ -41,7 +41,7 @@
 
 
 
-//     const geometry = new THREE.SphereGeometry( 15, 32, 16 );
+//     const geometry = new THREE.SpheredddGeometry( 15, 32, 16 );
 //     const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
 //     const sphere = new THREE.Mesh( geometry, material );
 //     sphere.position.set(0, 1, 1)
